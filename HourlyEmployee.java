678559f526da056fc3f26647a8a7d97004160794
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-09-14 20:00:51 +0700

    Exercise 1
    membuat subclass dari Employee, dan membuat methode overridding dari class parent

--- HourlyEmployee.java
+++ HourlyEmployee.java
@@ -1,1 +1,25 @@
+public class HourtlyEmployee extends Employee{
+    private int hoursWorked;
+    private int hourlyPayment;
 
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getHourlyPayment() {
+        return hourlyPayment;
+    }
+
+    public void setHourlyPayment(int hourlyPayment) {
+        this.hourlyPayment = hourlyPayment;
+    }
+
+    @Override
+    public int payment() {
+        return 0;
+    }
+}
