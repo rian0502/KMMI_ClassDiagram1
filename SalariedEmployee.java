20dc5fcb6f99881a63381c89ae4bfcc15fa279f0
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-09-14 20:01:52 +0700

    Exercise 1
    membuat subclass dari Employee, dan membuat methode overridding dari class parent

--- SalariedEmployee.java
+++ SalariedEmployee.java
@@ -1,1 +1,16 @@
+public class SalariedEmployee extends Employee{
+    private int salary;
 
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public int payment() {
+        return 0;
+    }
+}
