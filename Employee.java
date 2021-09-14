bfcec08639f3b4323a9ed35f9388c3a3cf3ebcab
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2021-09-14 19:59:03 +0700

    Exercise 1
    membuat abstract classs,dan membuat methode abstract yaitu payment

--- Employee.java
+++ Employee.java
@@ -1,1 +1,22 @@
+public abstract class Employee {
+    private String name;
+    private String afm;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+
+    public abstract int payment();
+}
