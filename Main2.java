a7a4be17ba70ff30aa1af609119ca3a9a2dc92d6
Miftahul Huda Guntara <90699049+hudalgtr99@users.noreply.github.com>
2021-09-18 13:53:39 +0700

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,5 +1,12 @@
-public class Main2.java {
-  public static void main(String[] args) {
-    Employee[] employee = {new SalariedEmployee(), new HourlyEmployee()};
-  }
+public class Main2{
+    public static void main(String[] args){
+        Employee[] em = {new SalariedEmployee(),new HourtlyEmployee()};
+    
+        for (int i = 0; i < 2; i++)
+            System.out.println(em[i].payment());
+        
+        em[0].setName("Nama ");
+        em[0].setAfm("Afm ");
+        em[0].setSalary(0);
+    }
 }
